b6cf166d310d2111cc5b41194a36038b8f8d6fa6
joxana <yohannesmeazaneh88@gmail.com>
2022-09-10 14:31:01 -0700

    sizeof different variable

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main function is first entry list
+*
+*
+*/
+int main(void)
+{
+	char c;
+	int  i;
+	long li;
+	long long lli;
+	float f;
+
+	printf("size of a char: %ld byte(s)\n", sizeof(c));
+	printf("size of an int: %ld byte(s)\n", sizeof(i));
+	printf("size of a long int: %ld byte(s)\n', sizeof(li));
+	printf("size of a long long int: %ld byte(s)n, sizeof(lli));
+	return (0)
+}
