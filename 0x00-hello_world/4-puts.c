92087a7a3e993c02b68be2344af96b6ad0216015
joxana <yohannesmeazaneh88@gmail.com>
2022-09-10 06:22:29 -0700

    commit betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,6 @@
 #include <stdio.h>
-/*
- *
- *  main - Entry point
- *
- *  
- *
- *  Return: Always 0 (Success)
- *
- */
-int main(void) /* main funnctionis an entry point*/
+int main(void) /* main is an entry point */
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
