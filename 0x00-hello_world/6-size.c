35141856dc89a0266940520cbdf8c003ac27be11
joxana <yohannesmeazaneh88@gmail.com>
2022-09-10 14:48:17 -0700

    size of data

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
-
 /**
-* main function is first entry list
 *
+*main is an entry point
 *
 */
 int main(void)
 {
 	char c;
-	int  i;
+	int i;
 	long li;
 	long long lli;
-	float f;
-
+	
 	printf("size of a char: %ld byte(s)\n", sizeof(c));
 	printf("size of an int: %ld byte(s)\n", sizeof(i));
-	printf("size of a long int: %ld byte(s)\n', sizeof(li));
-	printf("size of a long long int: %ld byte(s)n, sizeof(lli));
-	return (0)
+	printf("size of a long int: %ld byte(S)\n", sizeof(li);
+	printf("size of a long long int: %ld byte(s)\n,sizeof(lli));
+	retrun (0);
 }
+
