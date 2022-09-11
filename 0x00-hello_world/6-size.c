4ea340fbfc7039a25fb789bbf7dc9526031b0cda
joxana <yohannesmeazaneh88@gmail.com>
2022-09-11 04:03:13 -0700

    size of varianles

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,10 @@ int main(void)
 	long li;
 	long long lli;
 
-        printf("the size of char: %ld byte(s)", sizeof(c));
-	printf("the size of an int: %ld byte(s)", sizeof(i));
-	printf("the size of a long: %ld byte(s)", sizeof(li));
-	printf("the size of a long long: %ld byte(s)", sizeof(lli));
+        printf("the size of char: %ld byte(s)\n", sizeof(c));
+	printf("the size of an int: %ld byte(s)\n", sizeof(i));
+	printf("the size of a long: %ld byte(s)\n", sizeof(li));
+	printf("the size of a long long: %ld byte(s)\n", sizeof(lli));
 	return (0);
 }
        	
