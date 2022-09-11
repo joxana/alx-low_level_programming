9ff2ebfbb81f686507fc9cf94f2c6b9d2ac21437
joxana <yohannesmeazaneh88@gmail.com>
2022-09-11 04:35:01 -0700

    size of variables

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,11 +9,13 @@ int main(void)
 	int i;
 	long li;
 	long long lli;
+	float f;
 
         printf("size of char: %ld byte(s)\n", sizeof(c));
 	printf("size of an int: %ld byte(s)\n", sizeof(i));
 	printf("size of a long: %ld byte(s)\n", sizeof(li));
 	printf("size of a long long: %ld byte(s)\n", sizeof(lli));
+	printf("size of a float: %ld byte(s)\n", sizeof(f));
 	return (0);
 }
        	
