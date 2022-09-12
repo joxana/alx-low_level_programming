1fc3be8d09629a9b62aab6264abe6c02dd722f4b
joxana <yohannesmeazaneh88@gmail.com>
2022-09-12 13:29:45 -0700

    common README file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,8 +13,8 @@ int main(void)
 
         printf("size of char: %ld byte(s)\n", sizeof(c));
 	printf("size of an int: %ld byte(s)\n", sizeof(i));
-	printf("size of a long: %ld byte(s)\n", sizeof(li));
-	printf("size of a long long: %ld byte(s)\n", sizeof(lli));
+	printf("size of a long int: %ld byte(s)\n", sizeof(li));
+	printf("size of a long long int: %ld byte(s)\n", sizeof(lli));
 	printf("size of a float: %ld byte(s)\n", sizeof(f));
 	return (0);
 }
