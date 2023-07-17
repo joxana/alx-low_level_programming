e89425d13af7ff7c67bf2e5a9fc1b794af1278a4
joxana <yohannesmeazaneh88@gmail.com>
2023-07-17 06:53:40 -0700

    add a description

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(void) /*main function returns 0*/
 {
-/* prints \"Programming is like building a multilingual puzzle */
+  // prints the specified strings
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
