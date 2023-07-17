44e536367d4fcfd99678dc0b5c5c4943a3f604c4
joxana <yohannesmeazaneh88@gmail.com>
2023-07-17 08:51:58 -0700

    create a header documentation

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-int main(void)
 /**
- *function displays programmed scripts
- *
- *
+ * this function displays a specified caracter
  */
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
