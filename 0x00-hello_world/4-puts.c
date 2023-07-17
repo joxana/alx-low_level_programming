4f870c56887fdb4e5e1e2f32d61319408c385b7e
joxana <yohannesmeazaneh88@gmail.com>
2023-07-17 08:38:52 -0700

    nothing to say

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-int main(void) /*main function returns 0*/
+int main(void)
+/*
+ *function displays programmed scripts
+ *
+ *
+ */
 {
-  // prints the specified strings
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
