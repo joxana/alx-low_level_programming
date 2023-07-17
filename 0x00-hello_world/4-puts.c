14cd931462d37c39f196cad72ebaffaabf9fbc65
joxana <yohannesmeazaneh88@gmail.com>
2023-07-17 06:51:43 -0700

    write a program that prints as spcecified

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(void) /*main function returns 0*/
+{
+/* prints \"Programming is like building a multilingual puzzle */
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
