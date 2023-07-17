43b375c6fa5fb4ee94c017e22a3db7b1782d5f25
joxana <yohannesmeazaneh88@gmail.com>
2023-07-17 09:21:15 -0700

    made correction for betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * 
- * Description: C program that prints exactly "Programming is like building a multilingual puzzle
- * 
- * 
+ *
+ * Description: prints exactly "Programming is like building a multilingual
+ *
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
