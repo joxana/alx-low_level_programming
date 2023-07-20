43d41967be6b6f8f29bd2148332467c3e4d76306
joxana <yohannesmeazaneh88@gmail.com>
2023-07-20 02:21:52 -0700

    write a program that prints a random number every time it executes

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
@@ -17,15 +18,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf("%d is positive,n\n");
+		printf("%d is positive\n",n);
 	}
 	if (n < 0)
 	{
-		printf("%d is negative,n\n");
+		printf("%d is negative\n",n);
 	}
 	if (n == 0)
 	{
-		printf("%d is zero,n\n");
+		printf("%d is zero\n",n);
 	}
 	return (0);
 }
