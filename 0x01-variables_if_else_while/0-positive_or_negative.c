9f0bc0a0543c99a7b8eefc51b3265722255cb0d9
joxana <yohannesmeazaneh88@gmail.com>
2023-07-20 02:11:39 -0700

    betty style code added

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,14 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/**
+ * main - random number assigner
+ *
+ * Description:  assign a random number to the variable n each time
+ * Return: returns (0);
+ */
 int main(void)
 {
 	int n;
@@ -11,14 +16,16 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-	  {
-	    printf("%d is positive\n",n);
-	  }
-	else if (n == 0)
-	  {
-	    printf("%d is zero\n",n);
-	  }
-	else
-	  printf("%d is negative\n",n);
+	{
+		printf("%d is positive,n\n");
+	}
+	if (n < 0)
+	{
+		printf("%d is negative,n\n");
+	}
+	if (n == 0)
+	{
+		printf("%d is zero,n\n");
+	}
 	return (0);
 }
