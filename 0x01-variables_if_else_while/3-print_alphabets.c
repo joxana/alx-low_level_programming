7e367e79910f457b4f0043120461c86fb5bd9934
joxana <yohannesmeazaneh88@gmail.com>
2023-07-21 07:39:58 -0700

    a program prints the small and capital letters in 2 different lines

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - entry of the program
+ *
+ * Description: this program prints small letter
+ * Return: zero(success)
+ */
+int main(void)
+{
+char a;
+char b;
+for (a = 'a'; a <= 'z'; a++)
+{
+	putchar(a);
+}
+printf("\n");
+for (b = 'A'; b <= 'Z'; b++)
+{
+	putchar(b);
+}
+printf("\n");
+return (0);
+}
