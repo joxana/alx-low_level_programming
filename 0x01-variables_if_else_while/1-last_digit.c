fb4dc9c45100497437b110716f9a1a5419643cbd
joxana <yohannesmeazaneh88@gmail.com>
2023-07-21 02:28:21 -0700

    program determines last digit of a number and it relation b/n other numbers

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,14 +21,14 @@ if (last_digit > 5)
 	printf("Last digit of %d is %d and is greater than 5\n"
 	, n, last_digit);
 }
-if (n == 0)
+if (last_digit == 0)
 {
 	printf("Last digit of %d is %d and is zero\n", n, last_digit);
 }
-if (n < 0 && n != 0)
+if (last_digit < 6 && last_digit != 0)
 {
-	printf("Last digit of %d is %d and is and is and"
-"is less than 6 and not 0\n", n, last_digit);
+	printf("Last digit of %d is %d and is less than 6 and not 0\n"
+	, n, last_digit);
 }
 return (0);
 }
