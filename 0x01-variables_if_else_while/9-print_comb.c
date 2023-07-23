07346425d9b5de9f487f95b0a7b1664968e52587
joxana <yohannesmeazaneh88@gmail.com>
2023-07-23 06:22:26 -0700

    program prints decimal digits in between space and commas

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,12 +9,17 @@ int main(void)
 {
 	int n = 48;
 	int c = 44;
+
 	while (n >= 48 && n <= 57)
 	{
-		putchar(n);
+	putchar(n);
+	if (n != 57)
+	{
 		putchar(c);
-		putchar(' ');
-		n++;
 	}
+	putchar(' ');
+	n++;
+	}
+	putchar('\n');
 return (0);
 }
